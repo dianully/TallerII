127be7c61677cd5af5d4f231913baf9b129a4724
Francisco <fran92162@gmail.com>
2025-09-04 15:22:55 -0300

    Edicion de Vendedor y Login

--- PuntoDeVentaGameBox/Login.Designer.cs
+++ PuntoDeVentaGameBox/Login.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LUsuario = new System.Windows.Forms.Label();
+            this.LContraseña = new System.Windows.Forms.Label();
+            this.TxUsuario = new System.Windows.Forms.TextBox();
+            this.TxContraseña = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(177, 187);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 96);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LUsuario
+            // 
+            this.LUsuario.AutoSize = true;
+            this.LUsuario.Location = new System.Drawing.Point(309, 174);
+            this.LUsuario.Name = "LUsuario";
+            this.LUsuario.Size = new System.Drawing.Size(54, 16);
+            this.LUsuario.TabIndex = 1;
+            this.LUsuario.Text = "Usuario";
+            // 
+            // LContraseña
+            // 
+            this.LContraseña.AutoSize = true;
+            this.LContraseña.Location = new System.Drawing.Point(309, 242);
+            this.LContraseña.Name = "LContraseña";
+            this.LContraseña.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.LContraseña.Size = new System.Drawing.Size(76, 16);
+            this.LContraseña.TabIndex = 2;
+            this.LContraseña.Text = "Contraseña";
+            // 
+            // TxUsuario
+            // 
+            this.TxUsuario.Location = new System.Drawing.Point(312, 194);
+            this.TxUsuario.Name = "TxUsuario";
+            this.TxUsuario.Size = new System.Drawing.Size(100, 22);
+            this.TxUsuario.TabIndex = 3;
+            // 
+            // TxContraseña
+            // 
+            this.TxContraseña.Location = new System.Drawing.Point(312, 261);
+            this.TxContraseña.Name = "TxContraseña";
+            this.TxContraseña.Size = new System.Drawing.Size(100, 22);
+            this.TxContraseña.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.TxContraseña);
+            this.Controls.Add(this.TxUsuario);
+            this.Controls.Add(this.LContraseña);
+            this.Controls.Add(this.LUsuario);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.Text = "Inicio de Sesion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label LUsuario;
+        private System.Windows.Forms.Label LContraseña;
+        private System.Windows.Forms.TextBox TxUsuario;
+        private System.Windows.Forms.TextBox TxContraseña;
     }
 }
 
--- PuntoDeVentaGameBox/Vendedor.Designer.cs
+++ PuntoDeVentaGameBox/Vendedor.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LProducto = new System.Windows.Forms.Label();
+            this.LCantidad = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LProducto
+            // 
+            this.LProducto.AutoSize = true;
+            this.LProducto.Location = new System.Drawing.Point(212, 97);
+            this.LProducto.Name = "LProducto";
+            this.LProducto.Size = new System.Drawing.Size(61, 16);
+            this.LProducto.TabIndex = 0;
+            this.LProducto.Text = "Producto";
+            // 
+            // LCantidad
+            // 
+            this.LCantidad.AutoSize = true;
+            this.LCantidad.Location = new System.Drawing.Point(355, 97);
+            this.LCantidad.Name = "LCantidad";
+            this.LCantidad.Size = new System.Drawing.Size(61, 16);
+            this.LCantidad.TabIndex = 1;
+            this.LCantidad.Text = "Cantidad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cliente:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(215, 191);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(249, 66);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(215, 297);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Vendedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1347, 652);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LCantidad);
+            this.Controls.Add(this.LProducto);
+            this.Name = "Vendedor";
             this.Text = "Vendedor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LProducto;
+        private System.Windows.Forms.Label LCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
