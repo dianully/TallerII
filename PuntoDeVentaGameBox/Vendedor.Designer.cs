fd436865e92160d0da2f2b41cd883577ce167bb9
Francisco <fran92162@gmail.com>
2025-09-04 15:50:53 -0300

    Actualizacion de vistas

--- PuntoDeVentaGameBox/Vendedor.Designer.cs
+++ PuntoDeVentaGameBox/Vendedor.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.LProducto = new System.Windows.Forms.Label();
             this.LCantidad = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.LCliente = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Total = new System.Windows.Forms.Label();
+            this.gbCliente = new System.Windows.Forms.GroupBox();
+            this.BCobrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LProducto
@@ -54,14 +55,14 @@
             this.LCantidad.TabIndex = 1;
             this.LCantidad.Text = "Cantidad";
             // 
-            // label1
+            // LCliente
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(212, 149);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Cliente:";
+            this.LCliente.AutoSize = true;
+            this.LCliente.Location = new System.Drawing.Point(212, 149);
+            this.LCliente.Name = "LCliente";
+            this.LCliente.Size = new System.Drawing.Size(51, 16);
+            this.LCliente.TabIndex = 2;
+            this.LCliente.Text = "Cliente:";
             // 
             // panel1
             // 
@@ -70,29 +71,47 @@
             this.panel1.Size = new System.Drawing.Size(249, 66);
             this.panel1.TabIndex = 3;
             // 
-            // dataGridView1
+            // Total
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(215, 297);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 4;
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(212, 515);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(41, 16);
+            this.Total.TabIndex = 5;
+            this.Total.Text = "Total:";
+            // 
+            // gbCliente
+            // 
+            this.gbCliente.Location = new System.Drawing.Point(215, 301);
+            this.gbCliente.Name = "gbCliente";
+            this.gbCliente.Size = new System.Drawing.Size(378, 136);
+            this.gbCliente.TabIndex = 6;
+            this.gbCliente.TabStop = false;
+            this.gbCliente.Text = "Datos del Cliente";
+            // 
+            // BCobrar
+            // 
+            this.BCobrar.Location = new System.Drawing.Point(215, 552);
+            this.BCobrar.Name = "BCobrar";
+            this.BCobrar.Size = new System.Drawing.Size(75, 23);
+            this.BCobrar.TabIndex = 7;
+            this.BCobrar.Text = "Cobrar";
+            this.BCobrar.UseVisualStyleBackColor = true;
             // 
             // Vendedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1347, 652);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BCobrar);
+            this.Controls.Add(this.gbCliente);
+            this.Controls.Add(this.Total);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LCliente);
             this.Controls.Add(this.LCantidad);
             this.Controls.Add(this.LProducto);
             this.Name = "Vendedor";
-            this.Text = "Vendedor";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "LVendedor";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,8 +121,10 @@
 
         private System.Windows.Forms.Label LProducto;
         private System.Windows.Forms.Label LCantidad;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LCliente;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.GroupBox gbCliente;
+        private System.Windows.Forms.Button BCobrar;
     }
 }
