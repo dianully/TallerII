f72963df881bf3f411c6e64ada7d5c1b8c4d4cd6
Francisco <fran92162@gmail.com>
2025-09-22 11:53:29 -0300

    cambios

--- PuntoDeVentaGameBox/Administrador.cs
+++ PuntoDeVentaGameBox/Administrador.cs
@@ -19,6 +19,8 @@ namespace PuntoDeVentaGameBox
             lAdministrador.Text = $"{SesionUsuario.Nombre} {SesionUsuario.Apellido}";
         }
 
+        //
+
         private void button2_Click(object sender, EventArgs e)
         {
             
